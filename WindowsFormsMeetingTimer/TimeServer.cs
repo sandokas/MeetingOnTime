85ad51113fdf1f791f27c5e4a5d61bc4a2706aa7
David Santos <sandokas@gmail.com>
2018-02-11 14:32:43 +0000

    First iteration on TimeServer
    At least is building

---
+++ WindowsFormsMeetingTimer/TimeServer.cs
@@ -0,0 +1,24 @@
+﻿using org.meetingontime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsMeetingTimer
+{
+    class TimeServer
+    {
+        public MeetingTimer timer { get; set; }
+        public void StartServer()
+        {
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            newsock.Bind(localEndPoint);
+            newsock.Listen(10);
+            Socket client = newsock.Accept();
+        }
+    }
+}
