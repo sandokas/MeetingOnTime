004ff292ffbe91a3e3e95df1f42fdf504449b889
David Santos <sandokas@gmail.com>
2019-08-27 21:27:55 +0100

    Updated VS and .net Framework

---
+++ MeetingConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleTCP.SimpleTcpServer tcpServer = new SimpleTCP.SimpleTcpServer();
+            tcpServer.Start(51914);
+            tcpServer.BroadcastLine("This is a broadcast");
+            tcpServer.DataReceived += HandleReceivedData;
+        }
+        static void HandleReceivedData(object tcpServer, SimpleTCP.Message e)
+        {
+            Console.WriteLine(tcpServer.);
+        }
+    }
+}
